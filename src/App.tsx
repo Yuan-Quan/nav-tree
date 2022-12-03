7d4ca03202f42e8a8b88fb2726b40352cbc63365
Luna Child <metroyuan@gmail.com>
2022-12-03 19:15:36 +0800

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { CssBaseline } from '@mui/material';
 import React from 'react';
+import { BrowserRouter as BRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import { Main } from './pages/Main';
+import { NotFound } from './pages/Main copy';
 
 function App() {
   return (
     <div className="App">
-
+      <CssBaseline/>
+      <BRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />  
+          <Route path="*" element={<NotFound />} />  
+        </Routes>
+      </BRouter>
     </div>
   );
 }
