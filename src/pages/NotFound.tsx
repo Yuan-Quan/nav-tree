f06797ccb825c135b436588f9023fa57574165bf
Luna Child <metroyuan@gmail.com>
2022-12-03 19:16:47 +0800

    created some pages

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export const NotFound = () => {
+    return (
+        <div>
+            <h1>404 Page Not Found</h1>
+            <p> Check your URL, or contact the admin to fix this. </p>
+        </div>
+    )
+}
