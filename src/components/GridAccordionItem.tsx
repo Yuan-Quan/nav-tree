24cf9eabd34fc398600862395883eec011de2de8
Luna Child <metroyuan@gmail.com>
2022-12-06 12:05:12 +0800

    put papers in accordion

--- src/components/GridAccordionItem.tsx
+++ src/components/GridAccordionItem.tsx
@@ -1,22 +1,29 @@
+import * as React from 'react';
 import { Accordion, AccordionDetails, AccordionSummary, Typography } from "@mui/material"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { GridPapers } from "./GridPapers";
+import { AccordionContext } from "./GridAccordions";
 
 interface IAccordionItemProps {
-    expanded: string;
+    expanded: string | false;
     title: string;
-    secondary: string;
-    handleChange: (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => void;
+    secondary?: string;
 }
 
 export const GridAccordionItem = (props: IAccordionItemProps) => {
+    const {expanded, setExpanded} = React.useContext(AccordionContext);
+    const handleChange = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+        console.log("handleChange");
+        setExpanded(isExpanded ? panel : false);
+    };
+
     return (
-        <Accordion expanded={props.expanded === 'panel1'} onChange={()=>{props.handleChange('panel1')}}>
+        <Accordion expanded={expanded === props.title} onChange={handleChange(props.title)}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
         >
           <Typography sx={{ width: '33%', flexShrink: 0 }}>
-            props.title
+            {props.title}
           </Typography>
           <Typography sx={{ color: 'text.secondary' }}>{props.secondary}</Typography>
         </AccordionSummary>
--- src/components/GridDisplay.tsx
+++ src/components/GridDisplay.tsx
@@ -1,5 +1,5 @@
 import { Box, Paper } from "@mui/material"
-import GridAccordions from "./GridAccordions"
+import { GridAccordions } from "./GridAccordions"
 
 export const GridDisplay = () => {
     return (
